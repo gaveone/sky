b8a1f7f9f573c5f416c6540b833ed171922323cf
“gave” <adeun2020@gmail.com>
2024-11-15 04:53:49 -0800

    Some up

--- store/user.ts
+++ store/user.ts
@@ -1,26 +1,2 @@
-import { create } from "zustand";
-type State = {
-  count: number
 
-};
-
-type Action = {
-  increment:()=>void,
-  decrement: ()=>void,
-
-  
-};
-
-export const usePersonStore = create<State & Action>((set) => ({
-  count: 0,
-  increment: async () => {
-    await new Promise((resolve) => setTimeout(resolve, 1000)); 
-    set((state) => ({ count: state.count + 1 }))
-  },
-  decrement: async () => {
-    await new Promise((resolve) => setTimeout(resolve, 1000)); 
-    set((state) => ({ count: state.count - 1 }))
-  },
-}));
-  
 
